d70def7bebd241865ff746e10785a1d574b27693
naitickr <80129593+naitickr@users.noreply.github.com>
2022-01-28 19:57:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ Webcam.set({
 
 });
 
-camera = document.getElementById("camera");
+camera = document.getElementById('camera');
 
 Webcam.attach('#camera');
 
@@ -14,13 +14,27 @@ Webcam.attach('#camera');
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-document.getElementById("result").innerHTML = '<image id="captured_image" src='+data_uri+'"/>'
+document.getElementById("result").innerHTML = '<image id="captured_image" src="'+data_uri+'"/>'
     });
 }
-console.log('ml5 version:', ml5.version);
+
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/EktoUKtXf/model.json', modelLoaded);
 
 function modelLoaded(){
 console.log('Model Loaded !');
 }
 
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult( error,result){
+    if(error){
+        console.error(error);
+    }
+else{
+    console.log(result);
+    document.getElementById("result_object_name").innerHTML = result[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(3);
+}
+}
